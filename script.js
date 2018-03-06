b5d05799788553e9160e781cb0d983040fc05f8f
kanishk307 <ysj30797.kj@gmail.com>
2018-03-06 13:45:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ document.querySelector('.btn-hold').addEventListener('click',function(){
     //Display UI
     document.querySelector('#score-'+activePlayer).textContent = scores[activePlayer];
     
-    if(scores[activePlayer] >= 20) {
+    if(scores[activePlayer] >= 100) {
     
      document.querySelector('#player-'+activePlayer).textContent = 'Winner!';
     document.querySelector('.dice').style.display='none';
